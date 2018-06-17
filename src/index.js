3cfe30d9540c59a77b1f4ec5f92379d125c187b4
Adil Zeshan <adil@zeshan.org>
2018-06-17 20:08:39 +0100

    Populate table of books via an API call

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './index.css';
+import { getBooks } from './api/bookAPI';
+
+// Populate table of books via API call.
+
+getBooks().then(result => {
+  let booksBody = "";
+
+  result.forEach(book => {
+    booksBody += `<tr>
+    <td><a href="#" data-id="${book.id}" class="deleteBook">Delete</a></td>
+    <td>${book.id}</td>
+    <td>${book.author}</td>
+    <td>${book.book}</td>
+    <td>${book.genre}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('books').innerHTML = booksBody;
+});
