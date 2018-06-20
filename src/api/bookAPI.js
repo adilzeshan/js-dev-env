fabcf5c33e7ec55a9cf28b87368b14e3ce10a0f8
Adil Zeshan <adil@zeshan.org>
2018-06-20 01:27:08 +0100

    Create new API call to delete a book

--- src/api/bookAPI.js
+++ src/api/bookAPI.js
@@ -7,8 +7,21 @@ export function getBooks() {
   return get('books');
 }
 
+export function deleteBook(id) {
+  return del(`books/${id}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call function 'delete' as it is a reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
