ec8cc52b704c8ac75afb1c43909f2b19f4b3d6e2
Adil Zeshan <adil@zeshan.org>
2018-06-23 01:05:27 +0100

    Update base URL for production API by pointing to Heroku API instead

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   return getQueryStringParameterByName('useMockApi')
     ? 'http://localhost:3001/'
-    : '/';
+    : 'https://bookcraft.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
