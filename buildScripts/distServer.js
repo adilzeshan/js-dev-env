1f2f4af4b3dd9e3ba591769e18a55ccb94a55036
Adil Zeshan <adil@zeshan.org>
2018-06-23 01:13:51 +0100

    Remove GET route from local /dist server because of newer Heroku API

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,32 +15,6 @@ app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/books', function (request, response) {
-  // Hard coding for simplicity. Pretend this hits a real database.
-  response.json(
-    [
-      {
-        "id": 1,
-        "author": "Lord Dunsany",
-        "book": "The King of Elfland's Daughter",
-        "genre": "fantasy"
-      },
-      {
-        "id": 2,
-        "author": "JRR Tolkien",
-        "book": "The Lord of the Rings",
-        "genre": "fantasy"
-      },
-      {
-        "id": 3,
-        "author": "Lynn Flewelling",
-        "book": "The Bone Doll's Twin",
-        "genre": "fantasy"
-      }
-    ]
-  );
-});
-
 app.listen(port, function (error) {
   if (error) {
     console.log(error);
