db6286f62567fd7d6532872b9bf5c67ee029a19f
Adil Zeshan <adil@zeshan.org>
2018-06-25 13:17:36 +0100

    Add demonstration DOM test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,24 @@
 import { assert } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
     assert.strictEqual(true, true);
+  });
+});
+
+describe('index.html', () => {
+  it('should wax lyrical', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+
+    jsdom.env(index, function (err, window) {
+      const para = window.document.getElementsByTagName('p')[0];
+
+      assert.include(para.innerHTML, "rolling centuries");
+      done();
+
+      window.close();
+    });
   });
 });
